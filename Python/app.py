4836ac881e473a51b5d713834efadfa20c7ee350
Dawid Krysa <dawidkrysa2000@gmail.com>
2025-07-05 21:44:39 +0200

    Add Flask secret key

--- Python/app.py
+++ Python/app.py
@@ -11,6 +11,7 @@ load_dotenv()
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db/{os.getenv('POSTGRES_DB')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
 db.init_app(app)
 
